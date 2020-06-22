c39b3fd2d777207afb3bda54e076f8cc44dfdfff
ahmed021 <ahmedabiodun4@gmail.com>
2020-06-22 20:28:30 +0100

    modified the index.js, now sorts based on points

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ fs.readFile('leaderboard.json', (err, data) => {
 	if (err) throw err;
 	let parsedData = JSON.parse(data);
 
-	// sort the file based on points
+	// sort the file based on total points
+	parsedData.sort((a,b) => 
+		(a.totalPoints > b.totalPoints) ? -1 : ((b.totalPoints > a.totalPoints) ? 1 : 0));
 	
 	console.log(parsedData);
-})
+});
 
 
 console.log('This is after the read call..');
